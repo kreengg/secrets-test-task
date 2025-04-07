55d7ba0591d3961ba7f6e9acf2ba63288cd74412
Kirill Shugaev <kira.krut@yandex.ru>
2025-04-07 20:54:32 +0500

    add no cache headers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 from src.api import api_router
 
@@ -9,6 +9,14 @@ def create_app() -> FastAPI:
         docs_url="/api/docs",
         redoc_url="/api/redoc",
     )
+
+    @app.middleware("http")
+    async def add_no_cache_headers(request: Request, call_next):
+        response: Response = await call_next(request)
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
 
     app.include_router(api_router)
     return app
